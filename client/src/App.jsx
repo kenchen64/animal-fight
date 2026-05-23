27f84dcbe2168df17b692c9afabb070ddb1c9be3
kenchen64 <kenchen6464@gmail.com>
2026-05-24 00:14:21 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,38 +2,64 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate,
 } from "react-router-dom";
 
 import Login from "./pages/Login";
-
 import Register from "./pages/Register";
-
 import Select from "./pages/Select";
-
 import Game from "./pages/Game";
 
 export default function App() {
+  const token =
+    localStorage.getItem("token");
+
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* 登入 */}
         <Route
           path="/login"
           element={<Login />}
         />
 
+        {/* 註冊 */}
         <Route
           path="/register"
           element={<Register />}
         />
 
+        {/* 選角色 */}
         <Route
           path="/select"
-          element={<Select />}
+          element={
+            token ? (
+              <Select />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
         />
 
+        {/* 遊戲 */}
         <Route
-          path="/"
-          element={<Game />}
+          path="/game"
+          element={
+            token ? (
+              <Game />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+
+        {/* 預設導向 */}
+        <Route
+          path="*"
+          element={
+            <Navigate to="/login" />
+          }
         />
       </Routes>
     </BrowserRouter>
