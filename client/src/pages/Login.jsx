d6371932e74ab8192f721bb4c6b1eee024be9a89
kenchen64 <kenchen6464@gmail.com>
2026-05-23 20:14:41 +0800

    Add files via upload

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+export default function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const login = async () => {
+    const res = await axios.post(
+      'https://animal-fight.onrender.com/auth/login',
+      {
+        username,
+        password
+      }
+    )
+
+    localStorage.setItem(
+      'token',
+      res.data.token
+    )
+    localStorage.setItem(
+      'username',
+      username
+    )
+
+    window.location.href = '/'
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input
+        placeholder='username'
+        onChange={e => setUsername(e.target.value)}
+      />
+
+      <input
+        type='password'
+        placeholder='password'
+        onChange={e => setPassword(e.target.value)}
+      />
+      <button onClick={login}>
+        Login
+      </button>
+    </div>
+  )
+}
