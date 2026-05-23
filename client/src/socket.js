2544bd075fae7f9f03a119f4196aea1f8fcff7b9
kenchen64 <kenchen6464@gmail.com>
2026-05-23 23:43:51 +0800

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io(
   "https://animal-fight.onrender.com", {
-    transports: ["websocket"]}
+    transports: ['polling', 'websocket']}
 );
 
 export default socket;
