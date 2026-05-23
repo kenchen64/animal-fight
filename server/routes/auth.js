d3727342c6ce89ef60bd905f2f1664a9b6efbb2a
kenchen64 <kenchen6464@gmail.com>
2026-05-24 00:17:17 +0800

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,55 +1,96 @@
-const express = require('express')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const express = require("express");
 
-const Player = require('../models/Player')
+const bcrypt = require("bcryptjs");
 
-const router = express.Router()
+const jwt = require("jsonwebtoken");
 
-router.post('/register', async (req, res) => {
-  const { username, password } = req.body
+const Player =
+  require("../models/Player");
 
-  const hashed = await bcrypt.hash(password, 10)
+const router = express.Router();
 
-  const user = await Player.create({
-    username,
-    password: hashed
-  })
+router.post(
+  "/register",
+  async (req, res) => {
 
-  res.json(user)
-})
+    const {
+      username,
+      password,
+    } = req.body;
 
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body
+    const exist =
+      await Player.findOne({
+        username,
+      });
 
-  const user = await Player.findOne({ username })
+    if (exist) {
+      return res.status(400).json({
+        error: "帳號已存在",
+      });
+    }
 
-  if (!user) {
-    return res.status(400).json({
-      error: 'User not found'
-    })
+    const hashed =
+      await bcrypt.hash(
+        password,
+        10
+      );
+
+    const user =
+      await Player.create({
+        username,
+        password: hashed,
+      });
+
+    res.json(user);
   }
+);
 
-  const valid = await bcrypt.compare(
-    password,
-    user.password
-  )
+router.post(
+  "/login",
+  async (req, res) => {
 
-  if (!valid) {
-    return res.status(400).json({
-      error: 'Wrong password'
-    })
+    const {
+      username,
+      password,
+    } = req.body;
+
+    const user =
+      await Player.findOne({
+        username,
+      });
+
+    if (!user) {
+      return res.status(400).json({
+        error: "找不到玩家",
+      });
+    }
+
+    const valid =
+      await bcrypt.compare(
+        password,
+        user.password
+      );
+
+    if (!valid) {
+      return res.status(400).json({
+        error: "密碼錯誤",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username:
+          user.username,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.json({
+      token,
+      user,
+    });
   }
+);
 
-  const token = jwt.sign(
-    {
-      id: user._id,
-      username: user.username
-    },
-    process.env.JWT_SECRET
-  )
-
-  res.json({ token, user })
-})
-
-module.exports = router
+module.exports = router;
